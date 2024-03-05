96c1754298714853ad58c1fc87b9b57c07c54019
adbarnes <adbarnes@gmail.com>
2024-03-05 13:05:05 +0000

    remove base

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import react from "@astrojs/react";
 // https://astro.build/config
 export default defineConfig({
   site: "https://adbarnes.github.io/",
-  base: "/portfolio2024",
+
   integrations: [react()],
 });
